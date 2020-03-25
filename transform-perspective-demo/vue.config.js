70937d22ab2df3ead403a8422f411ac6db6d3b1d
guidetheorient <chrisji0518@outlook.com>
2020-03-25 16:59:16 +0800

    run yarn build

--- transform-perspective-demo/vue.config.js
+++ transform-perspective-demo/vue.config.js
@@ -1,5 +1,10 @@
+const path = require('path')
+const pathArr = __dirname.split(path.sep);
+let dirname = pathArr[pathArr.length - 1]
+let parentDirname = pathArr[pathArr.length - 2]
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/blog-demo/dist/'
+  ? `/${parentDirname}/${dirname}/dist/`
   : '/'
 }
